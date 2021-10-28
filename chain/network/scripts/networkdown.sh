a4adc4e1bb6a4db1c7b2df4902488be93cf5db69
Gioele Bigini <gioele.bigini@outlook.com>
2021-10-28 18:03:48 +0200

    Adding Smart Contract

--- chain/network/scripts/networkdown.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-pushd ~/mount/network/docker
-docker-compose -f ./docker-compose-test-net.yaml down
-docker volume prune -f
-popd
---
+++ client/core.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+#
+    
+if __name__ == "__main__":    
+    try:
+        # load network configuration
+        
+        # create CA client
+
+        # wallet for identity management
+
+        # enroll admin user
+        
+    except Exception as e:
+        print(e)
