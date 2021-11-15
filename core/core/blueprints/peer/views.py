154ef36c4e4cc575fd639fd6f9f6636c5b48a827
Gioele Bigini <gioele.bigini@outlook.com>
2021-11-15 12:01:10 +0100

    Updating Readme

--- core/core/blueprints/peer/views.py
+++ core/core/blueprints/peer/views.py
@@ -7,24 +7,24 @@ peer = Blueprint('peer', __name__, template_folder='templates')
 
 @peer.route('/getRequests', methods = ['GET'])
 def getRequests():
-    r = requests.get('http://192.168.33.10:4000/getPolls')
+    r = requests.get('http://93.56.200.143:4090/getPolls')
     return r.json(), 200
 
 @peer.route('/inFavour', methods = ['POST'])
 def inFavour():
     input_json = request.get_json(force=True) 
-    r = requests.post('http://192.168.33.10:4000/inFavour/'+input_json["request_id"])
+    r = requests.post('http://93.56.200.143:4090/inFavour/'+input_json["request_id"])
     return r.json(), 200
 
 @peer.route('/against', methods = ['POST'])
 def against():
     input_json = request.get_json(force=True) 
-    r = requests.post('http://192.168.33.10:4000/against/'+input_json["request_id"])
+    r = requests.post('http://93.56.200.143:4090/against/'+input_json["request_id"])
     return r.json(), 200
 
 @peer.route('/getFriends')
 def getFriends():
-    r = requests.get('http://192.168.33.10:4000/getFriends')
+    r = requests.get('http://93.56.200.143:4090/getFriends')
     return r.json(), 200
 
 @peer.route('/status', methods = ['GET'])
